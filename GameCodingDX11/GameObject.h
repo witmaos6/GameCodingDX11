1800613894fb805f704299345afc0b3cde3216f4
witmaos6 <62473633+witmaos6@users.noreply.github.com>
2023-12-29 21:50:56 +0900

    feat GameObject
    code refactoring

---
+++ GameCodingDX11/GameObject.h
@@ -0,0 +1,34 @@
+#pragma once
+class GameObject
+{
+public:
+	GameObject(ComPtr<ID3D11Device> device, ComPtr<ID3D11DeviceContext> deviceContext);
+	~GameObject();
+
+	void Update();
+	void Render(shared_ptr<Pipeline> pipeline);
+
+private:
+	ComPtr<ID3D11Device> _device;
+
+	shared_ptr<Geometry<VertexTextureData>> _geometry;
+	shared_ptr<VertexBuffer> _vertexBuffer;
+	shared_ptr<IndexBuffer> _indexBuffer;
+	shared_ptr<InputLayout> _inputLayout;
+
+	shared_ptr<VertexShader> _vertexShader;
+	shared_ptr<RasterizerState> _rasterizerState;
+	shared_ptr<PixelShader> _pixelShader;
+	shared_ptr<Texture> _texture;
+	shared_ptr<SamplerState> _samplerState;
+	shared_ptr<BlendState> _blendState;
+
+private:
+	TransformData _transformData; // 메시의 크기, 위치, 회전 값
+	shared_ptr<ConstantBuffer<TransformData>> _constantBuffer;
+
+	Vec3 _localPosition = { 0.f, 0.f, 0.f };
+	Vec3 _localRotation = { 0.f, 0.f, 0.f };
+	Vec3 _localScale = { 1.f, 1.f, 1.f };
+};
+
